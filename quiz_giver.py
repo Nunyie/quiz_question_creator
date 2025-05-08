6e6126f68f89859935193c4b9ba01f3689345f95
Nunyie <roswellbunag05@gmail.com>
2025-05-08 20:01:40 +0800

    Reads lines in txt file in groups of 6

--- quiz_giver.py
+++ quiz_giver.py
@@ -8,4 +8,11 @@ def load_quiz_data(filename='question_bank.txt'):
     with open(filename, 'r') as file:
         lines = [line.strip() for line in file if line.strip()]
 
-        
+    quiz_data = []
+    # Reads txt file in groups of 6 lines (1 question, 4 answers, and 1 correct answer)
+    for i in range(0, len(lines), 6):
+        block = lines[i:i+6]
+        if len(block) == 6 and block[0].startswith("Question:"):
+            quiz_data.append(block)
+
+    return quiz_data
