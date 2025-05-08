b3f66c4547d420d9466a19d7cc63b3cf2bf0c916
Nunyie <roswellbunag05@gmail.com>
2025-05-08 19:58:20 +0800

    Reads txt file from quiz_creator

--- quiz_giver.py
+++ quiz_giver.py
@@ -1,3 +1,11 @@
 # Nunyie
 # Quiz Giver
 # Reads the text file created by the quiz creator and quizzes the user
+
+import random
+# Reads the text file created by the quiz creator and quizzes the user
+def load_quiz_data(filename='question_bank.txt'):
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file if line.strip()]
+
+        
